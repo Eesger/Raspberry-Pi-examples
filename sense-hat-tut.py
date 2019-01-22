79aead90bb8e3f33739050237afeb55f021cd427
Eesger <Eesger@users.noreply.github.com>
2019-01-22 22:05:28 +0100

    Create sense-hat-tut.py

---
+++ sense-hat-tut.py
@@ -0,0 +1,16 @@
+from sense_hat import SenseHat
+from datetime import datetime
+
+sense = SenseHat()
+
+def get_sense_data():
+    sense_data = []
+    sense_data.append(sense.get_temperature())
+    sense_data.append(sense.get_pressure())
+    sense_data.append(sense.get_humidity())
+    sense_data.append(datetime.now())
+    
+    return sense_data
+
+while True:
+    print(get_sense_data())
